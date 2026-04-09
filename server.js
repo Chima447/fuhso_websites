0f8fea717d7068a60d39f61a986cced4cf2cd21b
chima447 <chimadaniel447@gmail.com>
2026-04-09 08:15:09 +0100

    change port to 10000 for unrender deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const indexRouter = require('./routes/indexRouter');
-const port = 3333;
+const port = 10000;
 
 
 
